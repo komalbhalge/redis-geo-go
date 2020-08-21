b13cfd3fe623cc954f36469fe1bbc1c47719e414
jeshocarmel <jeshocarmel@gmail.com>
2020-08-21 15:06:51 +0800

    removed packages and initiated go mod init

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/komalbhalge/redis-geo-go.git
+
+go 1.13
+
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/go-redis/redis/v8 v8.0.0-beta.7
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/komalbhalge/redis-geo-go v0.0.0-20200821065632-8f96f6f52912
+	golang.org/x/exp/errors v0.0.0-20200819202907-27b6b2ade93b
+)
--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
-	api "github.com/komalbhalge/redis-geo-go/api"
 )
 
 func main() {
@@ -17,7 +16,7 @@ func main() {
 }
 func setupRoutes() *httprouter.Router {
 	router := httprouter.New()
-	router.POST("/addlocation", api.AddLocation)
-	router.POST("/searchLocation", api.SearchLocation)
+	router.POST("/addlocation", AddLocation)
+	router.POST("/searchLocation", SearchLocation)
 	return router
 }
